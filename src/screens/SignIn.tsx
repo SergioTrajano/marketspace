ba92ea955a46223e44ab7751b2213268336f0275
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-05-21 03:35:31 -0300

    :sparkles: feat: integrate SignIn screen with backend
    - Control inputs with react-hook-forms;
    - Integrate screen with backend

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,19 +1,67 @@
-import { Center, Image, VStack } from "native-base";
+import { Center, Image, VStack, useToast } from "native-base";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { useNavigation } from "@react-navigation/native";
 
-import { Title } from "@components/Title";
-import { Text } from "@components/Text";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
+import { Text } from "@components/Text";
+import { Title } from "@components/Title";
 
 import Logo from "@assets/Logo.png";
 
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useAuth } from "@hooks/userAuth";
 import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
+import { signInSchema } from "@schemas/signInSchema";
+import { AppError } from "@utils/AppError";
+import { AxiosError } from "axios";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+
+type SignInProps = {
+    email: string;
+    password: string;
+};
 
 export function SignIn() {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
     const { navigate } = useNavigation<AuthNavigatorRoutesProps>();
+
+    const { signIn } = useAuth();
+    const toast = useToast();
+
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<SignInProps>({
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+        resolver: yupResolver(signInSchema),
+    });
+
+    async function handleSignIn(user: SignInProps) {
+        setIsLoading(true);
+        try {
+            await signIn(user.email, user.password);
+        } catch (error) {
+            const isAppError = error instanceof AppError;
+            const isAxiosError = error instanceof AxiosError;
+
+            let title = isAppError ? error.message : "Houve um erro. Tente novamente mais tarde!";
+
+            if (isAxiosError) {
+                title = error.response.data.message;
+            }
+
+            toast.show({ title, backgroundColor: "red.500", placement: "top" });
+        } finally {
+            setIsLoading(false);
+        }
+    }
 
     function handleNewAccount() {
         navigate("signUp");
@@ -57,18 +105,38 @@ export function SignIn() {
                             marginBottom={4}
                         />
 
-                        <Input
-                            placeholder="E-mail"
-                            height={11}
+                        <Controller
+                            control={control}
+                            name="email"
+                            render={({ field: { value, onChange } }) => (
+                                <Input
+                                    placeholder="E-mail"
+                                    height={11}
+                                    onChangeText={onChange}
+                                    value={value}
+                                    errorMessage={errors.email?.message}
+                                />
+                            )}
                         />
 
-                        <Input
-                            placeholder="Senha"
-                            inputType="PASSWORD"
-                            height={11}
+                        <Controller
+                            control={control}
+                            name="password"
+                            render={({ field: { value, onChange } }) => (
+                                <Input
+                                    placeholder="Senha"
+                                    inputType="PASSWORD"
+                                    height={11}
+                                    onChangeText={onChange}
+                                    value={value}
+                                    errorMessage={errors.password?.message}
+                                />
+                            )}
                         />
 
                         <Button
+                            onPress={handleSubmit(handleSignIn)}
+                            isLoading={isLoading}
                             buttonType="SECONDARY"
                             text="Entrar"
                             height={11}
