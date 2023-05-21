189fb3e6a9ca37345a23df346381b18b42412368
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-05-21 01:29:33 -0300

    :sparkles: feat: add signIn function to context

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,9 +1,15 @@
-import { createContext, ReactNode, useMemo, useState } from "react";
+import { UserDTO } from "@dtos/UserDTO";
+import { api } from "@services/api";
+import { authTokenStorage } from "@storages/storageAuthToken";
+import { userStorage } from "@storages/storageUser";
+import { createContext, ReactNode, useState } from "react";
 
 type AuthContextProps = {
-    user: string;
+    user: UserDTO;
     token: string;
-    LogOut: () => Promise<void>;
+    signIn: (email: string, password: string) => Promise<void>;
+    signOut: () => Promise<void>;
+    isLoadingStorageData: boolean;
 };
 
 type ProviderReactNode = {
@@ -13,12 +19,40 @@ type ProviderReactNode = {
 export const AuthContext = createContext<AuthContextProps>({} as AuthContextProps);
 
 export function AuthContextProvider({ children }: ProviderReactNode) {
-    const [user, setUser] = useState<string>("sd");
+    const [user, setUser] = useState<UserDTO>({} as UserDTO);
     const [token, setToken] = useState<string>("");
+    const [isLoadingStorageData, setIsLoadingStorageData] = useState<boolean>(false);
 
-    async function LogOut() {
-        setUser("");
+    async function signIn(email: string, password: string) {
+        try {
+            const { data } = await api.post("sessions", { email, password });
+
+            if (data.user && data.token) {
+                setIsLoadingStorageData(true);
+
+                updateUserAndTokenStates(data.user, data.token);
+
+                api.defaults.headers.common["Authorization"] = data.token;
+
+                await authTokenStorage.save(data.token);
+                await userStorage.save(data.user);
+            }
+        } catch (error) {
+            throw error;
+        } finally {
+            setIsLoadingStorageData(false);
+        }
+    }
+    async function signOut() {}
+
+    function updateUserAndTokenStates(user: UserDTO, token: string) {
+        setUser(user);
+        setToken(token);
     }
 
-    return <AuthContext.Provider value={{ user, token, LogOut }}>{children}</AuthContext.Provider>;
+    return (
+        <AuthContext.Provider value={{ user, token, signIn, signOut, isLoadingStorageData }}>
+            {children}
+        </AuthContext.Provider>
+    );
 }
