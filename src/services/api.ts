c89b001a301fe9c2f70a447249620bf310dc2cd2
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2023-05-26 05:56:28 -0300

    feat: :sparkles: Implementes refresh_token process

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,82 +1,60 @@
-import axios, { AxiosInstance } from "axios";
-
 import { API_URL } from "@env";
 
-import { authTokenStorage } from "@storages/storageAuthToken";
+import axios, { AxiosError, AxiosInstance } from "axios";
 
+import { authTokenStorage } from "@storages/storageAuthToken";
 import { AppError } from "@utils/AppError";
 
-type registerInterceptTokenManagerProps = {
-    signOut: () => void;
-    refreshedTokenUpdated: (newToken: string) => void;
-};
-
-type APIInstaceProps = AxiosInstance & {
-    registerInterceptTokenManager: ({
-        signOut,
-        refreshedTokenUpdated,
-    }: registerInterceptTokenManagerProps) => () => void;
-};
+type SignOut = () => void;
 
 type PromiseType = {
-    resolve: (value?: unknown) => void;
-    reject: (reason?: unknown) => void;
+    onSuccess: (token: string) => void;
+    onFailure: (error: AxiosError) => void;
 };
 
-type ProcessQueueParams = {
-    error: Error | null;
-    token: string | null;
+type APIInstanceProps = AxiosInstance & {
+    registerInterceptTokenManager: (signOut: SignOut) => () => void;
 };
 
 const api = axios.create({
     baseURL: API_URL,
-}) as APIInstaceProps;
+}) as APIInstanceProps;
 
+let failedQueued: Array<PromiseType> = [];
 let isRefreshing = false;
-let failedQueue: Array<PromiseType> = [];
 
-const process = ({ error, token = null }: ProcessQueueParams): void => {
-    failedQueue.forEach((request) => {
-        if (error) {
-            request.reject(error);
-        } else {
-            request.resolve(token);
-        }
-    });
-
-    failedQueue = [];
-};
-
-api.registerInterceptTokenManager = ({ signOut, refreshedTokenUpdated }) => {
+api.registerInterceptTokenManager = (singOut) => {
     const interceptTokenManager = api.interceptors.response.use(
         (response) => response,
         async (requestError) => {
-            if (requestError?.response?.status === 401) {
+            if (requestError.response?.status === 401) {
                 if (
                     requestError.response.data?.message === "token.expired" ||
                     requestError.response.data?.message === "token.invalid"
                 ) {
-                    const currentToken = await authTokenStorage.get();
+                    const { refreshToken } = await authTokenStorage.get();
 
-                    if (currentToken === null) {
-                        signOut();
+                    if (!refreshToken) {
+                        singOut();
                         return Promise.reject(requestError);
                     }
 
-                    const originalRequest = requestError.config;
+                    const originalRequestConfig = requestError.config;
 
                     if (isRefreshing) {
                         return new Promise((resolve, reject) => {
-                            failedQueue.push({ resolve, reject });
-                        })
-                            .then((token) => {
-                                originalRequest.headers["Authorization"] = `Bearer ${token}`;
-
-                                return axios(originalRequest);
-                            })
-                            .catch((error) => {
-                                throw error;
+                            failedQueued.push({
+                                onSuccess: (token: string) => {
+                                    originalRequestConfig.headers = {
+                                        Authorization: `Bearer ${token}`,
+                                    };
+                                    resolve(api(originalRequestConfig));
+                                },
+                                onFailure: (error: AxiosError) => {
+                                    reject(error);
+                                },
                             });
+                        });
                     }
 
                     isRefreshing = true;
@@ -84,30 +62,44 @@ api.registerInterceptTokenManager = ({ signOut, refreshedTokenUpdated }) => {
                     return new Promise(async (resolve, reject) => {
                         try {
                             const { data } = await api.post("/sessions/refresh-token", {
-                                token: currentToken,
+                                refresh_token: refreshToken,
                             });
 
-                            await authTokenStorage.save(data.token);
+                            await authTokenStorage.save({
+                                token: data.token,
+                                refreshToken: data.refresh_token,
+                            });
 
+                            if (originalRequestConfig.data) {
+                                originalRequestConfig.data = JSON.parse(originalRequestConfig.data);
+                            }
+
+                            originalRequestConfig.headers = {
+                                Authorization: `Bearer ${data.token}`,
+                            };
                             api.defaults.headers.common["Authorization"] = `Bearer ${data.token}`;
-                            originalRequest.headers["Authorization"] = `Bearer ${data.token}`;
 
-                            refreshedTokenUpdated(data.token);
+                            failedQueued.forEach((request) => {
+                                request.onSuccess(data.token);
+                            });
 
-                            process({ error: null, token: data.token });
-                            resolve(originalRequest);
+                            resolve(api(originalRequestConfig));
                         } catch (error: any) {
-                            process({ error, token: null });
-                            signOut();
+                            console.log(error);
+                            failedQueued.forEach((request) => {
+                                request.onFailure(error);
+                            });
 
+                            singOut();
                             reject(error);
                         } finally {
                             isRefreshing = false;
+                            failedQueued = [];
                         }
                     });
                 }
 
-                signOut();
+                singOut();
             }
 
             if (requestError.response && requestError.response.data) {
